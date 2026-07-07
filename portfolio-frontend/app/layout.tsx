904f006b0895cff7f754268909e456f8d3a46320
habcreative2026 <habcreative.portfolio.2026@gmail.com>
2026-07-07 14:54:11 +0700

    Update layout.tsx

--- portfolio-frontend/app/layout.tsx
+++ portfolio-frontend/app/layout.tsx
@@ -1,37 +1,33 @@
-// app/layout.tsx
 import "./globals.css";
 import ClientLayout from "./ClientLayout";
 import { LanguageProvider } from "./i18n/LanguageContext";
 import { LinkProvider } from "./context/LinkContext";
-import { IntroProvider } from "./context/IntroContext";
 import MaintenanceGuard from "./components/MaintenanceGuard";
 
 export const metadata = {
-  title: "HAB Creative",
-  description: "Mô tả",
-  icons: {
-    icon: "/icon.png?=999",
-  },
+title: "HAB Creative",
+description: "Mô tả",
+icons: {
+icon: "/icon.png?=999",
+},
 };
 
 export default function RootLayout({
-  children,
+children,
 }: {
-  children: React.ReactNode;
+children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body cz-shortcut-listen="true">
-        <MaintenanceGuard>
-          <IntroProvider>
-            <LinkProvider>
-              <LanguageProvider>
-                <ClientLayout>{children}</ClientLayout>
-              </LanguageProvider>
-            </LinkProvider>
-          </IntroProvider>
-        </MaintenanceGuard>
-      </body>
-    </html>
-  );
+return (
+<html lang="en">
+<body cz-shortcut-listen="true">
+<MaintenanceGuard>
+<LinkProvider>
+<LanguageProvider>
+<ClientLayout>{children}</ClientLayout>
+</LanguageProvider>
+</LinkProvider>
+</MaintenanceGuard>
+</body>
+</html>
+);
 }
